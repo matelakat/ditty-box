5aa6400e76a07e9bdbeab6a0048a67cefcbf8cc2
Mate Lakat <mlakat@mlakat.hu>
2013-09-22 19:24:37 +0100

    Use RawConfigParser

--- dittybox/config.py
+++ dittybox/config.py
@@ -15,7 +15,7 @@ class Section(object):
 class Configuration(object):
     def __init__(self, fp):
         self.fp = fp
-        self.config_parser = ConfigParser.SafeConfigParser()
+        self.config_parser = ConfigParser.RawConfigParser()
         self.config_parser.readfp(fp)
 
     def __getattr__(self, section):
