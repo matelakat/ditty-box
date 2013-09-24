166ffbe30019535a8c36961aa9a3f2c313ac9db7
Mate Lakat <mlakat@mlakat.hu>
2013-09-24 22:01:38 +0100

    Clean up files during soak up

--- soak_up.sh
+++ soak_up.sh
@@ -16,6 +16,11 @@ cat /mnt/ubuntu/root/install.log
 echo "[COLLECTED INFORMATION END]"
 
 rm -f /mnt/ubuntu/etc/init/install.conf
+rm -f /mnt/ubuntu/root/data.blob
+rm -f /mnt/ubuntu/root/install.stdout
+rm -f /mnt/ubuntu/root/install.stderr
+rm -f /mnt/ubuntu/root/install.log
+rm -f /mnt/ubuntu/root/install.sh
 
 while mount | grep "/mnt/ubuntu"; do
     umount /mnt/ubuntu | true
