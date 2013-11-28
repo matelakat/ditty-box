c04ee76deef0d259f5196e9c4858d09fa93424cb
Mate Lakat <mlakat@mlakat.hu>
2013-11-28 23:55:53 +0000

    implemented mount_point_of

--- dittybox/nginx_configurator.py
+++ dittybox/nginx_configurator.py
@@ -1,6 +1,19 @@
 class NginXConfigurator(object):
-    pass
+    def __init__(self, filesystem, config_root, nginx_config_bits):
+        self.config_root = config_root
+        self.filesystem = filesystem
+        self.nginx_config_bits = nginx_config_bits
+
+    def mount_point_of(self, identifier):
+        try:
+            mountpoint_target = self.filesystem.contents_of(
+                '/'.join([self.config_root, identifier]))
+            configuration = self.filesystem.contents_of(
+                mountpoint_target)
+            return mountpoint_target.split('/')[-1]
+        except:
+            return None
 
 
-def create():
-    pass
+def create(filesystem, config_root, nginx_config_bits):
+    return NginXConfigurator(filesystem, config_root, nginx_config_bits)
--- dittybox/test/test_nginx_configurator.py
+++ dittybox/test/test_nginx_configurator.py
@@ -1,7 +1,41 @@
 import unittest
 from dittybox import nginx_configurator
+from dittybox import filesystem
 
 
-class TestNginXConfigurator(unittest.TestCase):
-    def test_constructor(self):
-        configurator = nginx_configurator.create()
+class TestMountPointOf(unittest.TestCase):
+    def get_config(self, fs_contents={}, config_root='', nginx_config_bits=''):
+        fs = filesystem.FakeFilesystem()
+        fs.content_by_path = fs_contents
+
+        return nginx_configurator.create(fs, config_root, nginx_config_bits)
+
+    def test_empty(self):
+        configurator = self.get_config()
+
+        self.assertEquals(None, configurator.mount_point_of('something'))
+
+    def test_existing(self):
+        configurator = self.get_config(
+            config_root='/opt/resources',
+            nginx_config_bits='/opt/nginx',
+            fs_contents={
+                '/opt/resources/something': '/opt/nginx/mountpoint',
+                '/opt/nginx/mountpoint': 'ignore'
+            }
+        )
+
+        self.assertEquals(
+            'mountpoint', configurator.mount_point_of('something'))
+
+    def test_target_not_existing(self):
+        configurator = self.get_config(
+            config_root='/opt/resources',
+            nginx_config_bits='/opt/nginx',
+            fs_contents={
+                '/opt/resources/something': '/opt/nginx/mountpoint',
+            }
+        )
+
+        self.assertEquals(
+            None, configurator.mount_point_of('something'))
