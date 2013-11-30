19f796ea3ed1d86a454ba5701762f4ae4ef7b2a9
Mate Lakat <mlakat@mlakat.hu>
2013-11-30 17:21:36 +0000

    Set mountpoint implementation

--- dittybox/nginx_configurator.py
+++ dittybox/nginx_configurator.py
@@ -1,8 +1,38 @@
+class Result(object):
+    def __init__(self, succeeded, message=None):
+        self._succeeded = succeeded
+        self.message = message
+
+    @property
+    def succeeded(self):
+        return self._succeeded
+
+
+class Success(Result):
+    def __init__(self):
+        super(Success, self).__init__(True)
+
+
+class Failure(Result):
+    def __init__(self, message):
+        super(Failure, self).__init__(False, message)
+
+
+class SuccessListing(Success):
+    def __init__(self, mounts):
+        super(SuccessListing, self).__init__()
+        self.mounts = mounts
+
+
+
 class NginXConfigurator(object):
-    def __init__(self, filesystem, config_root, nginx_config_bits):
+    def __init__(self, filesystem, filesystem_manipulator, config_root,
+                 nginx_config_bits, config_generator):
         self.config_root = config_root
         self.filesystem = filesystem
         self.nginx_config_bits = nginx_config_bits
+        self.filesystem_manipulator = filesystem_manipulator
+        self.config_generator = config_generator
 
     def mount_point_of(self, identifier):
         try:
@@ -14,6 +44,34 @@ class NginXConfigurator(object):
         except:
             return None
 
+    def set_mountpoint(self, identifier, location):
+        resource_path = '/'.join([self.config_root, identifier])
+        location_path = '/'.join([self.nginx_config_bits, location])
+        try:
+            contents = self.filesystem.contents_of(location_path)
+            return Failure('%s already mounted' % location)
+        except:
+            pass
+        try:
+            self.filesystem_manipulator.write(
+                resource_path,
+                location_path)
+            self.filesystem_manipulator.write(
+                location_path,
+                self.config_generator(location))
+        except Exception as e:
+            return Failure(e.message)
+        return Success()
 
-def create(filesystem, config_root, nginx_config_bits):
-    return NginXConfigurator(filesystem, config_root, nginx_config_bits)
+    def list_mounts(self):
+        return SuccessListing([])
+
+
+def fake_config_generator(location):
+    return "config-for %s" % location
+
+
+def create(filesystem, filesystem_manipulator, config_root, nginx_config_bits,
+           config_generator):
+    return NginXConfigurator(
+        filesystem, filesystem_manipulator, config_root, nginx_config_bits, config_generator)
--- dittybox/test/test_nginx_configurator.py
+++ dittybox/test/test_nginx_configurator.py
@@ -1,15 +1,20 @@
 import unittest
 from dittybox import nginx_configurator
 from dittybox import filesystem
+from dittybox import filesystem_manipulator
 
 
-class TestMountPointOf(unittest.TestCase):
+class ConfigMixIn(object):
     def get_config(self, fs_contents={}, config_root='', nginx_config_bits=''):
         fs = filesystem.FakeFilesystem()
         fs.content_by_path = fs_contents
 
-        return nginx_configurator.create(fs, config_root, nginx_config_bits)
+        return nginx_configurator.create(
+            fs, filesystem_manipulator.Fake(), config_root, nginx_config_bits,
+            nginx_configurator.fake_config_generator)
 
+
+class TestMountPointOf(ConfigMixIn, unittest.TestCase):
     def test_empty(self):
         configurator = self.get_config()
 
@@ -39,3 +44,52 @@ class TestMountPointOf(unittest.TestCase):
 
         self.assertEquals(
             None, configurator.mount_point_of('something'))
+
+
+class TestSetMountPoint(unittest.TestCase, ConfigMixIn):
+    def test_setting_a_mountpoint(self):
+        config = self.get_config(
+            config_root='/opt/config', nginx_config_bits='/opt/nginx')
+
+        result = config.set_mountpoint('something', 'location')
+
+        self.assertEquals([
+            'write /opt/config/something /opt/nginx/location',
+            'write /opt/nginx/location config-for location',
+        ], config.filesystem_manipulator.executed_commands)
+
+        self.assertTrue(result.succeeded)
+
+    def test_location_already_mounted(self):
+        config = self.get_config(
+            fs_contents={
+                '/opt/nginx/location': 'something'
+            },
+            config_root='/opt/config', nginx_config_bits='/opt/nginx')
+
+        result = config.set_mountpoint('something', 'location')
+
+        self.assertFalse(result.succeeded)
+        self.assertEquals('location already mounted', result.message)
+
+    def test_exception_on_fs_operation(self):
+        config = self.get_config(
+            config_root='/opt/config', nginx_config_bits='/opt/nginx')
+
+        config.filesystem_manipulator.raise_errors = {
+            'write /opt/config/something /opt/nginx/location': Exception('msg')}
+
+        result = config.set_mountpoint('something', 'location')
+
+        self.assertFalse(result.succeeded)
+        self.assertEquals('msg', result.message)
+
+
+class TestListMountPoints(unittest.TestCase, ConfigMixIn):
+    def test_empty_list(self):
+        config = self.get_config()
+
+        result = config.list_mounts()
+
+        self.assertTrue(result.succeeded)
+        self.assertEquals([], result.mounts)
